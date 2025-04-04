171c5cab49dccb3d024049d25c33b0d6a8c5969d
nagendra kumar <rocky.naggi@gmail.com>
2025-04-04 20:15:05 +0530

    second commit2

--- loan_analysis/settings.py
+++ loan_analysis/settings.py
@@ -121,4 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['loan-analysis-approval.onrender.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com',  # Allow any subdomain of onrender.com
+]
